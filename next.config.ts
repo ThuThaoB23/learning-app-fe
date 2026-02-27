ace28e9aed2762daeb1f5eb1ff05586fd8d0e884
thuctd <thuctd28012002@gmail.com>
2026-02-27 10:14:34 +0700

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
 
+const internalApiBaseUrl = process.env.INTERNAL_API_BASE_URL ?? "http://app:8080";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${internalApiBaseUrl}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
